4cacfdc48b1ded925e283e2957c663b8893426ec
Ryan Lampe <42628235+rlampe2@users.noreply.github.com>
2019-11-09 11:41:27 -0600

    Removed comments/code

--- PowerLines.java
+++ PowerLines.java
@@ -31,12 +31,9 @@ public class PowerLines {
 		for(int i = 0; i < numPairs; i++) {
 			for(int j = 0; j < numPairs; j++) { //could be more efficient considering it's symmetric
 				if(i == j) {
-			//		adjM[i][j] = Double.MAX_VALUE; //is this correct?
 					adjM[i][j] = 0.0;
 				}else {
 					//find the distances and treat it as the weight. 
-				//	double temp = Math.pow((xCoord[i] - xCoord[j] ), 2);
-				//	double temp2 = Math.pow((yCoord[i] - yCoord[j]), 2);
 					adjM[i][j] = Math.sqrt(  Math.pow((xCoord[i] - xCoord[j] ), 2) + Math.pow((yCoord[i] - yCoord[j]), 2) );
 				}
 				
@@ -118,28 +115,14 @@ public class PowerLines {
 				
 			}
 				
-				//now that we have min, and we know that it connects 
-				//from MST to v not in mst, we can add the v and the v2, and the weight
-		//		if(findMax == 0) {
-		//		System.out.printf("%d to %2d,\t   %d\n", i, j, min);
-		//		}
-		//		else {
-		//			System.out.printf("%d to %2d,\t   %d\n", i, j, -1*min);
-		//		}
-			
-			
-				//add to our total weight
+			//add to our total weight
 				totalWeight += min;
 				//now that v2 is connected to the MST, 
 				//put it in the list, and take it out of the compliment
 				V_T.add(j);
 				complimentV_T.remove(Integer.valueOf(j)); //need this to remove the object, not at the index value 
-				
-			
-			
-			
+	
 		}
-		//System.out.printf("The total weight of the minimum spanning tree was: %d", totalWeight);
 		
 		
 		return totalWeight;
@@ -171,11 +154,6 @@ public class PowerLines {
 					 //get next line
 					 input =  reader.readLine();
 
-					 //Read in our two numbers
-//					 String s = idata.nextToken();
-
-		//			 int x = Integer.parseInt(idata.nextToken());
-		// y = Integer.parseInt(idata.nextToken());		
 					 String[] tokens = input.split(" ");
 					 int x = Integer.parseInt(tokens[0]);
 					 int y = Integer.parseInt(tokens[1]);
@@ -192,61 +170,7 @@ public class PowerLines {
 			 
 			 
 		 }
-	/*	 Scanner scan = new Scanner(System.in);
-		 int numPoints = scan.nextInt();
-		 
-		 //finding out when to stop reading in inputs
-		 while (numPoints != 0) {
-			 
-		 
-		 
-		 //build arrays to hold them:
-		 Integer[] xCoord = new Integer[numPoints];
-		 Integer[] yCoord = new Integer[numPoints];
-		 
-
-		 //Burn the endline character
-		 scan.nextLine();
-
-
-		 //actually process the input
-		 for(int i = 0; i < numPoints; i++) {
-			 //Read in our two numbers
-			 int x = scan.nextInt();
-			 int y = scan.nextInt();
-			 //burn the rest!
-			 scan.nextLine();
-			
-			 xCoord[i] = x;
-			 yCoord[i] = y;
-		 }
-		 //process current coordinate arrays and output
-		 Double[][] matrix = generateAdjMatrix(xCoord, yCoord, numPoints);
-		 double minWeight = genMST(matrix);
-		 System.out.printf("%.2f\n", minWeight);
-		 
-		 
-		 
-		 //get next set started
-		 numPoints = scan.nextInt();
-		 
-		 
-		 }
-		 scan.close();*/
-		 
-	/*	 //temp debugs:
-		 Integer[] x = {0, 0, 10 ,10};
-		 Integer[] y = {0, 10, 0, 10};
-		 Double[][] matrix = generateAdjMatrix(x, y, x.length);
-		 double min = genMST(matrix);
-		 System.out.printf("%f\n", min);
-
-		 Integer[] x2 = {0, 10};
-		 Integer[] y2 = {0, 10};
-		 matrix = generateAdjMatrix(x2, y2, x2.length);
-		 min = genMST(matrix);
-		 System.out.printf("%.2f\n", min);
-		 */
+	
 	}
 	 
 	}
